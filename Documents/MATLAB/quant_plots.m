81309b7427db19d69ee95fe651d6fa793bb8309e
ryan.batehup <ryan130393@hotmail.com>
2014-07-23 12:18:36 +1000

    'fnstat runs, first upload'

--- Documents/MATLAB/quant_plots.m
+++ Documents/MATLAB/quant_plots.m
@@ -371,3 +371,117 @@ for window=[31,61,91]
     close;
     
 end
+
+%% Density plots overlayed with Line correlation percentile plots
+for window=[31,61,91]
+    load(['DataFiles/runcorr',num2str(window),'yrwdw.mat'])
+    load(['DataFiles/nonstat_map',num2str(window),'yrwdw.mat'])
+% Temp
+bin = -1.0:0.02:1.0;
+[sorted_corr sorted_corr_ind] = sort(corr_ts(:));
+sorted_ts_runcorr = ts_runcorr(:,sorted_corr_ind);
+bin_sizes = histc(squeeze(sorted_corr),bin);
+current_index = 1; ts_runcorr_quan = nan(7,length(bin_sizes));
+for m=1:length(bin_sizes)
+    ts_runcorr_quan(:,m) = quantile(reshape(sorted_ts_runcorr(:,current_index:(current_index-1+bin_sizes(m))),[],1), ...
+                 [0.01,0.05,0.25,0.5,0.75,0.95,0.99]);
+    current_index = current_index + bin_sizes(m);
+end
+
+clf; axes; hold on; Hnd = nan(1,7); plot([0,-1;0,1],[-1,0;1,0],'k')
+for n=1:7
+    HA(n) = plot(bin,ts_runcorr_quan(n,:));
+    set(HA(n),'Color','k','LineWidth',2);
+end
+grid on; axis equal; axis([-1 1 -1 1]);
+set(HA([3,5]),'Visible','off')
+set(HA([1,7]),'LineStyle','-','LineWidth',1);
+set(HA(4),'LineWidth',3); 
+
+plot([-0.3,-0.3],[-1 1],'k'); plot([0.3,0.3],[-1 1],'k')
+plot([-1 1],[-0.3,-0.3],'k'); plot([-1 1],[0.3,0.3],'k')
+nonstat_map_ind = find(nonstat_tsmap > ceil(0.1*(NUM_YRS-window)) & ...
+    1 );
+% squeeze(abs(mean(ts_pc(1,(window+2):end,:,:)-ts_pc(2,(window+2):end,:,:),2))) > 0.3
+[a,b] = ind2sub(size(nonstat_tsmap),nonstat_map_ind);
+corr_3d = permute(repmat(corr_ts,[1 1 499]),[3,1,2]);
+runcr = nan(size(ts_runcorr)); corr_3d_fmt = nan(size(corr_3d));
+for i=1:length(a)
+    indices = find(nonstat_tsmaprecord(:,a(i),b(i)));
+    corr_3d_fmt(indices,a(i),b(i)) = corr_3d(indices,a(i),b(i));
+    runcr(indices,a(i),b(i)) = ts_runcorr(indices,a(i),b(i));
+end
+data1 = corr_3d_fmt;
+data2 = runcr;
+values = hist3([data1(:) data2(:)],{-1:0.01:1, -1:0.01:1});
+colormap(flipud(gray))
+pcolor(-1:0.01:1,-1:0.01:1,values'); shading flat
+colorbar
+caxis([0,100])
+axis equal
+xlim([-1,1]); ylim([-1, 1]);
+ylabel(['Running Correlations (using ',num2str(window),' yr windows)']); xlabel('Correlations over 499 yr period');
+title(['Correlation percentiles for modeled temperature - rcor:',num2str(window),'yr'])
+set(gcf, 'PaperUnits', 'centimeters'); % May already be default
+set(gcf, 'PaperPosition', [0 0 19 28]); %x_width=19cm y_width=28cm
+saveas(gcf,['Plots/hist3(corr_ts_3d,ts_runcorr)_rcor_nstat+lines',num2str(window),'.jpg'])
+xlim([-1,1]); ylim([-1, 1]); hold off;
+
+
+
+
+% Precip
+
+bin = -1.0:0.02:1.0;
+[sorted_corr sorted_corr_ind] = sort(corr_pr(:));
+sorted_pr_runcorr = pr_runcorr(:,sorted_corr_ind);
+bin_sizes = histc(squeeze(sorted_corr),bin);
+current_index = 1; pr_runcorr_quan = nan(7,length(bin_sizes));
+for m=1:length(bin_sizes)
+    pr_runcorr_quan(:,m) = quantile(reshape(sorted_pr_runcorr(:,current_index:(current_index-1+bin_sizes(m))),[],1), ...
+                 [0.01,0.05,0.25,0.5,0.75,0.95,0.99]);
+    current_index = current_index + bin_sizes(m);
+end
+
+% The plotting part
+
+clf; axes; hold on; Hnd = nan(1,7); plot([0,-1;0,1],[-1,0;1,0],'k')
+for n=1:7
+    HA(n) = plot(bin,pr_runcorr_quan(n,:));
+    set(HA(n),'Color','k','LineWidth',2);
+end
+grid on; axis equal; axis([-1 1 -1 1]);
+set(HA([1,7]),'LineStyle','-','LineWidth',1);
+set(HA([3,5]),'Visible','off')
+set(HA(4),'LineWidth',3);
+
+plot([-0.3,-0.3],[-1 1],'k'); plot([0.3,0.3],[-1 1],'k')
+plot([-1 1],[-0.3,-0.3],'k'); plot([-1 1],[0.3,0.3],'k')
+nonstat_map_ind = find(nonstat_prmap > ceil(0.1*(NUM_YRS-window)) & ...
+    1 );
+% squeeze(abs(mean(pr_pc(1,(window+2):end,:,:)-pr_pc(2,(window+2):end,:,:),2))) > 0.3
+[a,b] = ind2sub(size(nonstat_prmap),nonstat_map_ind);
+corr_3d = permute(repmat(corr_pr,[1 1 499]),[3,1,2]);
+runcr = nan(size(pr_runcorr)); corr_3d_fmt = nan(size(corr_3d));
+for i=1:length(a)
+    indices = find(nonstat_prmaprecord(:,a(i),b(i)));
+    corr_3d_fmt(indices,a(i),b(i)) = corr_3d(indices,a(i),b(i));
+    runcr(indices,a(i),b(i)) = pr_runcorr(indices,a(i),b(i));
+end
+data1 = corr_3d_fmt;
+data2 = runcr;
+values = hist3([data1(:) data2(:)],{-1:0.01:1, -1:0.01:1});
+colormap(flipud(gray))
+caxis([0,100])
+pcolor(-1:0.01:1,-1:0.01:1,values'); shading flat
+colorbar
+axis equal
+xlim([-1,1]); ylim([-1, 1]);
+ylabel(['Running Correlations (using ',num2str(window),' yr windows)']); xlabel('Correlations over 499 yr period');
+title(['Correlation percentiles for modeled precipitation - rcor:',num2str(window),'yr'])
+set(gcf, 'PaperUnits', 'centimeters'); % May already be default
+set(gcf, 'PaperPosition', [0 0 19 28]); %x_width=19cm y_width=28cm
+saveas(gcf,['Plots/hist3(corr_pr_3d,pr_runcorr)_rcor_nstat+lines',num2str(window),'.jpg'])
+xlim([-1,1]); ylim([-1, 1]); hold off;
+close;
+end
